04965dd9ec70923fe7facc64d99e8424318fbb9a
Wind Scar <windscaar13@gmail.com>
2019-12-05 20:26:28 +0530

    Player jump changes
    Player jump changes added

--- Simple 3D/Assets/Scripts/PlayerMovement.cs
+++ Simple 3D/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public Transform playerTransform;
     public Rigidbody rigidBody;
     public float forwardForce = 1000f;
     public float sidewayForce = 500f;
+    public float upwardForce = 500f;
 
     public string moveRight = "d";
     public string moveLeft = "a";
+
+    public bool objInGround = true;
 
 
     // Update is called once per frame
     // Use FixedUpdate instead of Update when u play with physics
     void FixedUpdate()
     {
+        
         rigidBody.AddForce(0,0, forwardForce * Time.deltaTime);
+
+        if(playerTransform.position.y >= 1.1)
+        {
+            objInGround = false;
+        }
+        else
+        {
+            objInGround = true;
+        }
 
         if (Input.GetKey(moveLeft))
         {
@@ -26,6 +39,13 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("d"))
         {
             rigidBody.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (objInGround)
+            {
+                rigidBody.AddForce(0, upwardForce, 0);
+            }
         }
         if(rigidBody.position.y < -1f)
         {
